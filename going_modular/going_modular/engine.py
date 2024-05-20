efc716f2a6740b491fbeff410c655100fcd28dd1
Nafiseh Mirnia <71519929+nafiseh1425@users.noreply.github.com>
2024-05-20 09:00:39 -0600

    Update engine.py

--- going_modular/going_modular/engine.py
+++ going_modular/going_modular/engine.py
@@ -116,9 +116,61 @@ def test_step(model: torch.nn.Module,
     test_acc = test_acc / len(dataloader)
     return test_loss, test_acc
 
+## validation
+def valid_step(model: torch.nn.Module, 
+              dataloader: torch.utils.data.DataLoader, 
+              loss_fn: torch.nn.Module,
+              device: torch.device) -> Tuple[float, float]:
+    """valids a PyTorch model for a single epoch.
+
+    Turns a target PyTorch model to "eval" mode and then performs
+    a forward pass on a testing dataset.
+
+    Args:
+    model: A PyTorch model to be tested.
+    dataloader: A DataLoader instance for the model to be tested on.
+    loss_fn: A PyTorch loss function to calculate loss on the test data.
+    device: A target device to compute on (e.g. "cuda" or "cpu").
+
+    Returns:
+    A tuple of testing loss and testing accuracy metrics.
+    In the form (test_loss, test_accuracy). For example:
+
+    (0.0223, 0.8985)
+    """
+    # Put model in eval mode
+    model.eval() 
+
+    # Setup test loss and test accuracy values
+    valid_loss, valid_acc = 0, 0
+
+    # Turn on inference context manager
+    with torch.inference_mode():
+        # Loop through DataLoader batches
+        for batch, (X, y) in enumerate(dataloader):
+            # Send data to target device
+            X, y = X.to(device), y.to(device)
+
+            # 1. Forward pass
+            valid_pred_logits = model(X)
+
+            # 2. Calculate and accumulate loss
+            loss = loss_fn(valid_pred_logits, y)
+            valid_loss += loss.item()
+
+            # Calculate and accumulate accuracy
+            valid_pred_labels = valid_pred_logits.argmax(dim=1)
+            valid_acc += ((valid_pred_labels == y).sum().item()/len(valid_pred_labels))
+
+    # Adjust metrics to get average loss and accuracy per batch 
+    valid_loss = valid_loss / len(dataloader)
+    valid_acc = valid_acc / len(dataloader)
+    return valid_loss, valid_acc
+####
 def train(model: torch.nn.Module, 
           train_dataloader: torch.utils.data.DataLoader, 
           test_dataloader: torch.utils.data.DataLoader, 
+          valid_dataloader:torch.utils.data.DataLoader
           optimizer: torch.optim.Optimizer,
           loss_fn: torch.nn.Module,
           epochs: int,
@@ -175,6 +227,11 @@ def train(model: torch.nn.Module,
           dataloader=test_dataloader,
           loss_fn=loss_fn,
           device=device)
+        
+        valid_loss, valid_acc = valid_step(model=model,
+          dataloader=valid_dataloader,
+          loss_fn=loss_fn,
+          device=device)
 
         # Print out what's happening
         print(
@@ -182,7 +239,9 @@ def train(model: torch.nn.Module,
           f"train_loss: {train_loss:.4f} | "
           f"train_acc: {train_acc:.4f} | "
           f"test_loss: {test_loss:.4f} | "
-          f"test_acc: {test_acc:.4f}"
+          f"test_acc: {test_acc:.4f}|"
+          f"valid_loss: {valid_loss:.4f}|"
+          f"valid_acc: {valid_acc:.4f}"
         )
 
         # Update results dictionary
@@ -190,6 +249,8 @@ def train(model: torch.nn.Module,
         results["train_acc"].append(train_acc)
         results["test_loss"].append(test_loss)
         results["test_acc"].append(test_acc)
+        results["valid_loss"].append(valid_loss)
+        results["valid_acc"].append(valid_acc)
 
     # Return the filled results at the end of the epochs
     return results
